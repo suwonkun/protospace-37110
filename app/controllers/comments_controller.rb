469568a241d760725cab0cdbe8ebd5a5b83f089a
suwonkun <92506153+suwonkun@users.noreply.github.com>
2021-11-02 18:56:32 +0900

    修正依頼箇所の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype) # 今回の実装には関係ありませんが、このようにPrefixでパスを指定することが望ましいです。
     else
-      @Prototype = @comment.prototype
+      @prototype = @comment.prototype
       @comments = @prototype.comments
       render "prototypes/show" # views/tweets/show.html.erbのファイルを参照しています。
     end
